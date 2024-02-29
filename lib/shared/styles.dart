59a8d1c1a17d296bff421e5637858b16eb9cd6bd
Harun Nurahman <harun.nurrahman@hotmail.com>
2024-02-29 12:34:16 +0700

    Style: importing assets and create styles.dart

---
+++ lib/shared/styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color pallete
+Color whiteColor = const Color(0xFFFFFFFF);
+Color blackColor = const Color(0xFF14193F);
+Color lightGrayColor = const Color(0xFFF1F1F9);
+Color grayColor = const Color(0xFFA4A8AE);
+Color blueColor = const Color(0xFF5142E6);
+Color navyColor = const Color(0xFF1A1D2E);
+Color cyanColor = const Color(0xFF53C1F9);
+Color greenColor = const Color(0xFF22B07D);
+
+Color lightBackgroundColor = const Color(0xFFF6F8FB);
+Color darkBackgroundColor = const Color(0xFF020518);
+
+// Text style
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle grayTextStyle = GoogleFonts.poppins(color: grayColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor);
+TextStyle blueTextStyle = GoogleFonts.poppins(color: cyanColor);
+
+// Font weight
+FontWeight light = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
